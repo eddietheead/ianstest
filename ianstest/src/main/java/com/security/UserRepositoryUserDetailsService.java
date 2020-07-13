c373003dee4124898bc12259299bd3bdc22c96a3
ian edwards <idedwards29@gmail.com>
2020-07-13 21:23:53 +0100

    corrected some import statements

--- ianstest/src/main/java/com/security/UserRepositoryUserDetailsService.java
+++ ianstest/src/main/java/com/security/UserRepositoryUserDetailsService.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.userdetails.
                                        UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import tacos.User;
-import tacos.data.UserRepository;
+import com.User;
+import com.data.UserRepository;
 
 @Service
 public class UserRepositoryUserDetailsService 
